88977770068bbd4bfc91668f2f558dcd9a41c8a1
theaanderschou <74912677+theaanderschou@users.noreply.github.com>
2021-04-19 16:04:32 +0200

    burger menu

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+// BURGER MENU
+const navslide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click',()=>{
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+
+        //Burger Animation
+        burger.classList.toggle('toggle');
+    });
+
+}
+
+navslide();
+
+
 // Her bliver der lavet consts til vores forskellige billeder
 // En const til det aktive billede og vores liste af billeder
 
@@ -41,3 +69,64 @@ document.querySelector(".plus-but").addEventListener("click", function () {
 
         document.getElementById("antal").value = valueCount;
 });
+
+
+
+
+// CONTACT FORM
+console.log("hej");
+const btn = document.getElementById('btn')
+const messageBox = document.getElementById("popup-besked");
+const checkImg = document.getElementById("checkImg");
+const okay = document.getElementById("confirm-btn");
+
+btn.addEventListener("click", function () {
+    const fname = document.getElementById('fname');
+    const email = document.getElementById('email');
+    const subject = document.getElementById('subject');
+    const message = document.getElementById('message');
+    const popup = document.getElementById("popup");
+
+    //Tilføjer active class på pop up, og indsætter tekst efter inputs
+    popup.classList.add("active");
+
+    if (fname.value == "") {
+        messageBox.innerText = "Indtast venligst dit fulde navn";
+        fname.focus();
+        return false;
+    }
+
+    if (email.value == "") {
+        messageBox.innerText = "Indtast venligst e-mail";
+        email.focus();
+        return false;
+    }
+
+    if (subject.value == "") {
+        messageBox.innerText = "Indtast venligst emne";
+        contry.focus();
+        return false;
+    }
+
+    if (message.value == "") {
+        messageBox.innerText = "Indtast besked";
+        message.focus();
+        return false;
+    }
+
+    else {
+        messageBox.innerText = "Vi har modtaget din besked";
+        checkImg.style.visibility = "visible";
+        okay.style.visibility = "hidden";
+    }
+
+
+    //Det nummer her fortæller hvor lang delayen er i ms
+    setTimeout(function () {
+        window.location.reload();
+    }, 4000)
+})
+
+okay.addEventListener("click", function(){
+    popup.classList.remove("active");
+})
