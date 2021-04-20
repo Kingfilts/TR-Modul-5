d586308977897f2a997a31710a93b22474d5cad9
tjobz <worlt100@gmail.com>
2021-04-20 19:46:12 +0200

    asd

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const navslide = () => {
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
 
-    burger.addEventListener('click',()=>{
+    burger.addEventListener('click', () => {
         //Toggle Nav
         nav.classList.toggle('nav-active');
 
@@ -12,7 +12,7 @@ const navslide = () => {
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = ''
-            }else{
+            } else {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
             }
         });
@@ -27,38 +27,40 @@ navslide();
 
 
 
-// Her bliver der lavet consts til vores forskellige billeder
-// En const til det aktive billede og vores liste af billeder
+function productpage() {
 
-const aktivBillede = document.querySelector(".product-hero-images .active");
-const produktBillede = document.querySelectorAll(".product-image-list img");
-// Herefter laves der en funktion som tager vores aktiv billede og skifter
-// ud med det billede der er blevet trykket på
-function skiftBillede(e) {
-    aktivBillede.src = e.target.src;
-}
+    // Her bliver der lavet consts til vores forskellige billeder
+    // En const til det aktive billede og vores liste af billeder
 
-// Når der bliver trykket på en af billederne på listen, vil der blive
-// kaldt til en funktion der sørger for at billedet skifter
+    const aktivBillede = document.querySelector(".product-hero-images .active");
+    const produktBillede = document.querySelectorAll(".product-image-list img");
+    // Herefter laves der en funktion som tager vores aktiv billede og skifter
+    // ud med det billede der er blevet trykket på
+    function skiftBillede(e) {
+        aktivBillede.src = e.target.src;
+    }
 
-produktBillede.forEach(image =>
-    image.addEventListener("click", skiftBillede));
+    // Når der bliver trykket på en af billederne på listen, vil der blive
+    // kaldt til en funktion der sørger for at billedet skifter
+
+    produktBillede.forEach(image =>
+        image.addEventListener("click", skiftBillede));
 
 
 
-// Vi laver en variabel til det samlede antal
-var valueCount
+    // Vi laver en variabel til det samlede antal
+    var valueCount
 
-// Increment at vores værdi når vi trykker på plus knappen. Der bliver
-// Lyttet efter et klik som udfører følgende funktion og tilføjer 1
-document.querySelector(".plus-but").addEventListener("click", function () {
+    // Increment at vores værdi når vi trykker på plus knappen. Der bliver
+    // Lyttet efter et klik som udfører følgende funktion og tilføjer 1
+    document.querySelector(".plus-but").addEventListener("click", function () {
 
-    valueCount = document.getElementById("antal").value;
+        valueCount = document.getElementById("antal").value;
 
-    valueCount++;
+        valueCount++;
 
-    document.getElementById("antal").value = valueCount;
-});
+        document.getElementById("antal").value = valueCount;
+    });
 
 
 
@@ -69,27 +71,26 @@ document.querySelector(".plus-but").addEventListener("click", function () {
         valueCount--;
 
         document.getElementById("antal").value = valueCount;
-});
+    });
 
 
-
+}
 
 
 
 
 // CONTACT FORM
-console.log("hej");
-const btn = document.getElementById('btn')
-const messageBox = document.getElementById("popup-besked");
-const checkImg = document.getElementById("checkImg");
-const okay = document.getElementById("confirm-btn");
+var btn = document.getElementById('send-form')
+var messageBox = document.getElementById("popup-besked");
+var checkImg = document.getElementById("checkImg");
+var okay = document.getElementById("confirm-btn");
 
 btn.addEventListener("click", function () {
-    const fname = document.getElementById('fname');
-    const email = document.getElementById('email');
-    const subject = document.getElementById('subject');
-    const message = document.getElementById('message');
-    const popup = document.getElementById("popup");
+    var fname = document.getElementById('fname');
+    var email = document.getElementById('email');
+    var subject = document.getElementById('subject');
+    var message = document.getElementById('message');
+    var popup = document.getElementById("popup");
 
     //Tilføjer active class på pop up, og indsætter tekst efter inputs
     popup.classList.add("active");
@@ -116,9 +117,7 @@ btn.addEventListener("click", function () {
         messageBox.innerText = "Indtast besked";
         message.focus();
         return false;
-    }
-
-    else {
+    } else {
         messageBox.innerText = "Vi har modtaget din besked";
         checkImg.style.visibility = "visible";
         okay.style.visibility = "hidden";
@@ -131,7 +130,7 @@ btn.addEventListener("click", function () {
     }, 4000)
 })
 
-okay.addEventListener("click", function(){
+okay.addEventListener("click", function () {
     popup.classList.remove("active");
 })
 
@@ -143,34 +142,32 @@ okay.addEventListener("click", function(){
 
 
 
-
-
 // Filtrering
 function getSelectValue(obj) {
-  var uid = obj.options[obj.selectedIndex].getAttribute('value');
-  //alert(uid);
+    var uid = obj.options[obj.selectedIndex].getAttribute('value');
+    //alert(uid);
 
-  var allProducts = document.querySelectorAll(".webshop-product")
-  var showThis = document.querySelectorAll(".webshop-product." + uid)
+    var allProducts = document.querySelectorAll(".webshop-product")
+    var showThis = document.querySelectorAll(".webshop-product." + uid)
 
-// Gemmer alle produkter væk ved filtrering
-  for (i = 0; i < allProducts.length; i++) {
-    allProducts[i].classList.add("hidden");
-  };
-
-// fjerner hidden class fra valgt filter
-  for (i = 0; i < showThis.length; i++) {
-    showThis[i].classList.remove("hidden");
-  }
-
-// viser alle produkter når "alle" filtrering er valgt
-  if (uid == "alle") {
+    // Gemmer alle produkter væk ved filtrering
     for (i = 0; i < allProducts.length; i++) {
-      allProducts[i].classList.remove("hidden");
+        allProducts[i].classList.add("hidden");
     };
-  }
+
+    // fjerner hidden class fra valgt filter
+    for (i = 0; i < showThis.length; i++) {
+        showThis[i].classList.remove("hidden");
+    }
+
+    // viser alle produkter når "alle" filtrering er valgt
+    if (uid == "alle") {
+        for (i = 0; i < allProducts.length; i++) {
+            allProducts[i].classList.remove("hidden");
+        };
+    }
 
 }
 
 
-changeImage()
+
